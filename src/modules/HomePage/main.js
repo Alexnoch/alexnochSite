68b204c4a18d15e11c2b7bad786db758ac29c79a
Alexnoch <jacksparrow-111111@mail.ru>
2021-08-07 22:37:26 +0300

    V1.1

--- src/modules/HomePage/main.js
+++ src/modules/HomePage/main.js
@@ -1,12 +1,23 @@
  import ImageTitle from '../../static/images/homepageTitle.jpg';
  import './style.css';
  const HomePage = (props)=>{
-    const { text } = props;
+   
 
     return(
         <div className="homepage-container">
             <img className="homepage-container-image" src={ImageTitle} />
-            {text}
+            <div className="homepage-conitaner-absolute">
+                <div className="homepage-container-relative">
+                    <div className="homepage-container-header">
+                        <h1 className="homepage-container-header-title">Александр Ночь</h1>
+                        <p className="homepage-container-header-skills">Сайты, дизайн, разработка</p>
+                        <p className="homepage-container-header-skills">html</p>
+                        <p className="homepage-container-header-skills">css</p>
+                        <p className="homepage-container-header-skills">javascript</p>
+                        <p className="homepage-container-header-skills">figma</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
