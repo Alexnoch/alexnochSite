f2c14fd40430aa9c3746ec53204ad7234b970431
Alexnoch <jacksparrow-111111@mail.ru>
2021-08-08 00:12:12 +0300

    V1.2

---
+++ src/modules/HomePage/components/Rubric/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export const Rubric = () =>{
+    return <div className="rubric-background">
+                <div className="rubric-container">
+                    <div className="rubric-header">
+                        <h2>Добро пожаловать!</h2>
+                        <p>Сайт включает в себя несколько категорий, это — статьи, мои работы, обо мне. 
+                            В каждом разделе уникальная информация из моего личного опыта, с которой 
+                            я охотно поделюсь с тобой, мой дорогой друг!</p>
+                    </div>
+                </div>
+            </div>
+}
--- src/modules/HomePage/main.js
+++ src/modules/HomePage/main.js
@@ -1,23 +1,27 @@
  import ImageTitle from '../../static/images/homepageTitle.jpg';
+ import { Rubric } from './components/Rubric/index';
  import './style.css';
+
  const HomePage = (props)=>{
    
-
     return(
-        <div className="homepage-container">
-            <img className="homepage-container-image" src={ImageTitle} />
-            <div className="homepage-conitaner-absolute">
-                <div className="homepage-container-relative">
-                    <div className="homepage-container-header">
-                        <h1 className="homepage-container-header-title">Александр Ночь</h1>
-                        <p className="homepage-container-header-skills">Сайты, дизайн, разработка</p>
-                        <p className="homepage-container-header-skills">html</p>
-                        <p className="homepage-container-header-skills">css</p>
-                        <p className="homepage-container-header-skills">javascript</p>
-                        <p className="homepage-container-header-skills">figma</p>
+        <div>
+            <div className="homepage-container">
+                <img className="homepage-container-image" src={ImageTitle} />
+                <div className="homepage-conitaner-absolute">
+                    <div className="homepage-container-relative">
+                        <div className="homepage-container-header">
+                            <h1 className="homepage-container-header-title">Александр Ночь</h1>
+                            <p className="homepage-container-header-skills">Сайты, дизайн, разработка</p>
+                            <p className="homepage-container-header-skills">html</p>
+                            <p className="homepage-container-header-skills">css</p>
+                            <p className="homepage-container-header-skills">javascript</p>
+                            <p className="homepage-container-header-skills">figma</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Rubric />
         </div>
     )
 }
