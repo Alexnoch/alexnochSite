c469912ccd3029be7b314da4f01887016afff384
Alexnoch <jacksparrow-111111@mail.ru>
2021-08-20 00:57:32 +0300

    Добавил функционала к разделу категории

--- src/modules/HomePage/components/Categories/index.js
+++ src/modules/HomePage/components/Categories/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import { SoftDeveloper } from './../SoftDeveloper';
 import './style.css';
 export const Categories = (props) => {
 
+const [ activeCategory, setActiveCategory ] = useState('SoftDeveloper');
 
     return <div className="categories-background">
         <div className="categories-container">
@@ -11,11 +13,11 @@ export const Categories = (props) => {
                 <p className="categories-container-header-text">Выбор</p>
             </div>
             <div className="categories-container-menu">
-                <button className="categories-container-menu-button">Личный опыт</button>
-                <button className="categories-container-menu-button">Софт разработчика</button>
-                <button className="categories-container-menu-button">Новости</button>
+                <button className={classnames("categories-container-menu-button", {categories_active:activeCategory === 'PersonalExp'})} onClick={()=>setActiveCategory('PersonalExp')}>Личный опыт</button>
+                <button className={classnames("categories-container-menu-button", {categories_active:activeCategory === 'SoftDeveloper'})} onClick={()=>setActiveCategory('SoftDeveloper')}>Софт разработчика</button>
+                <button className={classnames("categories-container-menu-button", {categories_active:activeCategory === 'Category_news'})} onClick={()=>setActiveCategory('PersonalExp')}>Новости</button>
             </div>
-            <SoftDeveloper />
+            {activeCategory === 'SoftDeveloper' ? <SoftDeveloper /> : '' }
         </div>
     </div>
 }
